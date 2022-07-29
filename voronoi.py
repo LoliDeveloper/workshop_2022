f94a44daf971c306cade0d75e1b27665a381c84f
LoliDevelover <43130034+LoliDeveloper@users.noreply.github.com>
2022-07-29 21:22:26 +0500

    Update voronoi.py

--- voronoi.py
+++ voronoi.py
@@ -19,13 +19,3 @@ region_polys, region_pts = voronoi_regions_from_coords(c, s)
 fig, ax = subplot_for_map(figsize=(18, 16))
 plot_voronoi_polys_with_points_in_area(ax, s, region_polys, c, region_pts)
 plt.show()
-
-def get_voronoi_graph(vertices, boundary_edges):
-    v_graph = [[[]]]
-    
-    return v_graph
-
-
-def print_voronoi(v_graph):
-    fig,ax = plt.subplots()
-    plt.show()
