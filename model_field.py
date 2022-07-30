01196652127ddead2bdfb303214568a46a5f3583
LoliDevelover <43130034+LoliDeveloper@users.noreply.github.com>
2022-07-30 11:07:10 +0500

    Update model field

--- model_field.py
+++ model_field.py
@@ -21,5 +21,17 @@ z = list(map(float, data[2]))
 fig, (ax1) = plt.subplots()
 ax1.plot([x[i] for i in range(len(x))], [y[i] for i in range(len(y))])
 ax1.scatter([z[0] for z in well_vertices], [z[1] for z in well_vertices])
+
+y_max = max(y)
+x_max = max(x)
+y_min = min(y)
+x_min = min(x)
+a_square = max(y_max - y_min, x_max - x_min)
+x_min_coord = x_min
+x_max_coord = x_min_coord + a_square
+y_min_coord = y_min
+y_max_coord = y_min_coord + a_square
+plt.xlim(x_min_coord, x_max_coord)
+plt.ylim(y_min_coord, y_max_coord)
 np.save("boundary", [[x[i],y[i]] for i in range(len(x))])
 plt.show()
